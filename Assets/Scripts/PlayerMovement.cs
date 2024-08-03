4152591f35eaf9da07fbd04606b5cc866b5c9b85
Caique Felipe Xavier <caique.felipexavier123@gmail.com>
2024-08-03 13:16:08 -0300

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float mouseSensitivity = 100.0f;
+    public Transform playerCamera;
+    public float cameraPitch = 0.0f;
+    public float maxLookAngle = 85.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked; // Trava o cursor no centro da tela
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleCamera();
+    }
+
+    void HandleMovement()
+    {
+        if (controller.isGrounded)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+            moveDirection = new Vector3(moveX, 0.0f, moveZ);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void HandleCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -maxLookAngle, maxLookAngle);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
